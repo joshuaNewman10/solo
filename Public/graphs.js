20dbc310412b7d7a113b7d18da16ecee2ea5af08
Hack Reactor Students <accounts@hackreactor.com>
2015-03-09 12:25:06 -0700

    Set up d3 graph file

--- Public/graphs.js
+++ Public/graphs.js
@@ -1,5 +1,6 @@
-
-
+/************************************
+Data
+*************************************/
 var fakeData = [
   {county: 'San Francisco', year: 2000, waterUse: {agriculture: 0.27, people: 0.25, industry: 0.28}},
   {county: 'San Francisco', year: 2005, waterUse: {agriculture: 0.27, people: 0.25, industry: 0.28}},
@@ -9,4 +10,38 @@ var fakeData = [
   {county: 'Sacramento', year: 2005, waterUse: {agriculture: 0.17, people: 0.42, industry: 0.34}}
 ];
 
+
+/************************************
+D3 Config
+*************************************/
+var config = {
+  margin: {top: 20, right: 20, bottom:30, left: 40},
+};
+config.width = 960 - config.margin.left - config.margin.right,
+config.height = 500 - config.margin.top - config.margin.bottom
+
+
+
+var xValue; // data-->value
+var xScale = d3.scale.linear().range([0, config.width]); //value-->display
+var xMap = function(d) {return xScale(xValue(d))}; //data-->display
+var xAxis = d3.svg.axis().scale(xScale).orient('bottom');
+
+var yValue; // data-->value
+var yScale = d3.scale.linear().range([0, config.width]); //value-->display
+var yMap = function(d) {return yScale(yValue(d))}; //data-->display
+var yAxis = d3.svg.axis().scale(xScale).orient('bottom');
+
+var cValue = function(d) {return d.waterUse.agriculture;};
+var color = d3.scaleCategory10();
+
+
+/************************************
+SVG Config
+*************************************/
+var svg = d3.select('.chart').append('svg')
+            .attr('width', config.width + config.margin.left + config.margin.right)
+            .attr('height', config.height + config.margin.top + config.margin.bottom)
+            .append('g')
+            .attr('transform', 'translate(' + config.margin.left + ',' + config.margin.top + ')');
 
