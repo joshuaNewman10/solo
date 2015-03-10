5e0dbeb125bc699624812f75ae947d6cb4f1e516
Joshua Newman <jonewman1020@gmail.com>
2015-03-09 22:10:10 -0700

    CSV importing and parsing working, fixed waterUse datafile

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var request = require('request');
 var mongoose = require('mongoose');
 var promise = require('bluebird');
-var csv = require('ya-csv');
+var csv = require('fast-csv');
 
 var app = express();
 app.use(express.static(__dirname + '/public'));
@@ -10,18 +10,38 @@ app.use(express.static(__dirname + '/public'));
 console.log('Server is listening on 8000');
 app.listen(8000);
 
-var loadCSVData = function() { //read in csv data
-  var reader = csv.createCsvStreamReader(process.openStdin());
-  reader.addListener('Public/Cereal.csv', function(data) {
-    console.log(data, 'hello?');
-  });
-  reader.addListener('end', function() {
-    console.log('done');
-  })
+var dataStore = {
 };
 
-app.get('Cereal', function(req, resp) {
+var parseData = function(data, path, cb) {
+  var headings = data[0];
+  var body = data.slice(1);
+  var objData = body.map(function(entry) {
+   var obj = {};
+   entry.forEach(function(val, i) {
+     obj[headings[i]] = val; 
+   });
+   return obj;
+  });
+  dataStore[path] = objData;
+  cb();
+};
 
+var importData = function(path, cb) {
+  var csvData = [];
+  csv
+  .fromPath(path)
+  .on('data', function(data) {
+    csvData.push(data);
+  })
+  .on('end', function() {
+    console.log('done reading csv data');
+    parseData(csvData,path, cb);
+  });
+};
+
+
+app.get('Cereal', function(req, resp) {
 });
 
 app.get('County', function(req, resp) {
@@ -32,7 +52,14 @@ app.get('TimeCounty', function(req, resp) {
 
 });
 
+importData('Public/Cereal.csv', function() {
+  console.log('logging datastore');
+  console.log(dataStore);
+});
+importData('Public/waterUse.csv', function() {
+  console.log('logging datastore');
+  console.log(dataStore);
+});
 
 
 
-loadCSVData();
